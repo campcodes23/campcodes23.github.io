3c887097044568d78ce10364fb84c460279f0f9e
Haley Keglovits <haley.k@berkeley.edu>
2023-07-20 22:21:44 -0400

    small

--- assets/datasets/p.py
+++ assets/datasets/p.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('air.csv')
+df = pd.read_csv('https://campcodes23.github.io/assets/datasets/spotify.csv')
+df.head(300).to_csv('spotify_small.csv')
 
-
-
-
+# eof
 
